82813cccc82a94052dc7897cb4506f56bd58d5a4
Ahfaz2003 <huzaifamehmood890@gmail.com>
2025-05-21 14:10:42 +0500

    spalsh screen done and all bugs are fixed

--- src/Utils/SplashScreen.js
+++ src/Utils/SplashScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { View, Image, StyleSheet, Dimensions } from 'react-native';
+import { View, Image, StyleSheet, Dimensions, ImageBackground } from 'react-native';
 
 const SplashScreen = ({ navigation }) => {
   useEffect(() => {
@@ -11,13 +11,17 @@ const SplashScreen = ({ navigation }) => {
   }, [navigation]);
 
   return (
-    <View style={styles.container}>
+    
+    <ImageBackground
+      source={require('../assets/SplashScreen/BgSplash.png')} // your background image
+      style={styles.background}
+    >
       <Image
-        source={require('../assets/splash.gif')}
-        style={styles.image}
+        source={require('../assets/SplashScreen/Logo.png')} // add your logo file here
+        style={styles.logo}
         resizeMode="contain"
       />
-    </View>
+    </ImageBackground>
   );
 };
 
@@ -26,14 +30,15 @@ export default SplashScreen;
 const { width, height } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
-    backgroundColor: 'red', // your splash background
+    width: width,
+    height: height,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  image: {
-    width: width * 0.8,
-    height: height * 0.8,
+    logo: {
+    width: width * 0.7,
+    height: height * 0.3,
   },
 });
